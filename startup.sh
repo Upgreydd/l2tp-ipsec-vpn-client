323d3969248efcf95250998aa84c5fc3ce91761f
Chris Wood <chriswood.ca@gmail.com>
2019-05-31 15:06:11 -0700

    changed startup to explicitly configure left side of ipsec

--- startup.sh
+++ startup.sh
@@ -2,7 +2,8 @@
 
 # template out all the config files using env vars
 sed -i 's/right=.*/right='$VPN_SERVER_IPV4'/' /etc/ipsec.conf
-echo ': PSK "'$VPN_PSK'"' > /etc/ipsec.secrets
+sed -i 's/left=.*/left='$VPN_CLIENT_IPV4'/' /etc/ipsec.conf
+echo '"'$VPN_CLIENT_IPV4'" "'$VPN_SERVER_IPV4'": PSK "'$VPN_PSK'"' > /etc/ipsec.secrets
 sed -i 's/lns = .*/lns = '$VPN_SERVER_IPV4'/' /etc/xl2tpd/xl2tpd.conf
 sed -i 's/name .*/name '$VPN_USERNAME'/' /etc/ppp/options.l2tpd.client
 sed -i 's/password .*/password '$VPN_PASSWORD'/' /etc/ppp/options.l2tpd.client
